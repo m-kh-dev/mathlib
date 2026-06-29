711808db353c745799884183370ecbaf69a13d2c
unknown <mkhdev10@gmail.com>
2026-06-29 11:48:32 +0330

    added Vectors and related things

--- simplemathlib.py
+++ simplemathlib.py
@@ -1,7 +1,7 @@
 #this is a library ive been working on for quite a bit
 #its a basic non bloated but probably slow math library
 from typing import List, Any, Optional
-from math import atan, sin, cos, sqrt
+from math import atan, sin, cos, sqrt, acos
 import ctypes as ct
 
 REAL = float|int
@@ -40,26 +40,26 @@ class Complex():
     def __init__(self, R : Optional[REAL], C : Optional[REAL]):
         self.a = R if (R!=None) else 0.0
         self.b = C if (C!=None) else 0.0
-    def __add__(self, other):
+    def __add__(self : Complex, other : Complex):
         if(isinstance(other,REAL)):
             return Complex(self.a + other, self.b)
         if(isinstance(other,(Complex))):
             return Complex(self.a + other.a, self.b + other.b)
-    def __sub__(self, other):
+    def __sub__(self : Complex, other : Complex):
         return self.__add__(-other)
-    def __iadd__(self, other):
+    def __iadd__(self : Complex, other : Complex):
         self = self.__add__(other)
-    def __isub__(self, other):
+    def __isub__(self : Complex, other : Complex):
         self = self.__sub__(other)
-    def __radd__(self, other):
+    def __radd__(self : Complex, other : Complex):
         return self.__add__(other)
-    def __rsub__(self, other):
+    def __rsub__(self : Complex, other : Complex):
         return self.__sub__(other)
     def __neg__(self):
         return Complex(-self.a, -self.b)
     def __invert__(self):
         return self.conjugate()
-    def __mul__(self, other):
+    def __mul__(self : Complex, other : Complex):
         if(isinstance(other,REAL)):
             return Complex(self.a * other, self.b * other)
         if(isinstance(other,(Complex))):
@@ -67,47 +67,47 @@ class Complex():
                 (self.a * other.a) - (self.b * other.b), 
                 (self.a * other.b) + (self.b * other.a)
                 )
-    def __imul__(self, other):
+    def __imul__(self : Complex, other : Complex):
         self = self.__mul__(other)
-    def __int__(self):
+    def __int__(self : Complex):
         return int(self.a)
-    def __float__(self):
+    def __float__(self : Complex):
         return (self.a)
-    def toVector(self):
+    def toVector(self : Complex):
         return Vector([self.a, self.b])
-    def argument(self):
+    def argument(self : Complex):
         return atan(self.b/self.a)
-    def flip(self):
+    def flip(self : Complex):
         return Complex(self.b, self.a)
-    def real(self):
+    def real(self : Complex):
         return self.a
-    def imaginery(self):
+    def imaginery(self : Complex):
         return self.b
-    def conjugate(self):
+    def conjugate(self : Complex):
         return Complex(self.a, -self.b)
-    def __str__(self):
+    def __str__(self : Complex):
         return(f"""{self.a} + {self.b}i""")
     def fromAngle(a : Optional[REAL], r : Optional[REAL]):
         return (Complex(cos(a),sin(a))*r)
-    def __abs__(self):
+    def __abs__(self : Complex):
         return sqrt(self.a**2 + self.b**2)
 
-def toVector(self):
+def toVector(self : Complex):
     return Vector([self.a, self.b])
-def argument(self):
+def argument(self : Complex):
     return atan(self.b/self.a)
-def flip(self):
+def flip(self : Complex):
     return Complex(self.b, self.a)
-def real(self):
+def real(self : Complex):
     return self.a
-def complex(self):
+def complex(self : Complex):
     return self.b
-def conjugate(self):
+def conjugate(self : Complex):
     return Complex(self.a, -self.b)
 def ComplexfromAngle(a : Optional[REAL], r : Optional[REAL]):
     return (Complex(cos(a),sin(a))*r)
 
-
+#liner algebra
 class Vector():
     def __init__(self, dtype : Optional[REAL], dim : REAL): 
         self.dtype = dtype
@@ -123,28 +123,25 @@ class Vector():
     def __setitem__(self, key, value): 
         if(isinstance(value,self.dtype)):
             self.array[key] = value
-    def __mul__(self, other):
+    def __mul__(self : Vector, other):
         if(isinstance(other, REAL)):
             temp = self.array.copy()
             for n in range(self.dim):
                 temp[n]*=other
         elif(isinstance(other, Vector)):
-            if(other.dim == self.dim):
-                temp = self.array.copy()
-                for n in range(self.dim):
-                    temp[n]*=other[n]
+            return self.innerProduct(other)
         else:
             raise ArithmeticError("unsupported opration")
-        return temp
-    def __str__(self):
+        return Vector(temp)
+    def __str__(self : Vector):
         return str(self.array)
-    def innerProduct(self, other):
+    def innerProduct(self : Vector, other : Vector):
+        temp = 0
         if(isinstance(other,Vector)):
-            temp = 0
             for i in range(self.dim):
                 temp += self[i]*other[i]
         return temp
-    def __add__(self, other):
+    def __add__(self : Vector, other):
         if(isinstance(other, Vector)):
             if(other.dim == self.dim):
                 temp = self.array.copy()
@@ -152,46 +149,53 @@ class Vector():
                     temp[n]+=other[n]
         else:
             raise ArithmeticError("unsupported opration")
-        return temp
-    def __neg__(self):
+        return Vector(temp)
+    def __neg__(self : Vector):
         temp = self.array.copy()
         for n in range(self.dim):
             temp[n] = -temp[n]
-        return temp
-    def __sub__(self, other):
+        return Vector(temp)
+    def __sub__(self : Vector, other):
         return self.__add__(-other)
-    def __mod__(self, other):
+    def __mod__(self : Vector, other : Vector):
         return self.innerProduct(other)
-    def __iadd__(self, other):
-        self = self + other
-    def __isub__(self, other):
-        self = self - other
-    def __imul__(self, other):
-        self = self*other
-    def __itruediv__(self, other):
-        self = self/other
-    def __truediv__(self, other):
+    def __iadd__(self : Vector, other):
+        self.array = (self + other).array
+        return self
+    def __isub__(self : Vector, other):
+        self.array = (self - other).array
+        return self
+    def __imul__(self : Vector, other):
+        self.array = (self*other).array
+        return self
+    def __itruediv__(self : Vector, other):
+        self.array = (self/other).array
+        return self
+    def __truediv__(self : Vector, other):
         if(isinstance(other,REAL)):
             return self*(1/other)
-    def __radd__(self, other):
+    def __radd__(self : Vector, other):
         return(self.__add__(other))
-    def __rsub__(self, other):
+    def __rsub__(self : Vector, other):
         return(self.__sub__(other))
-    def __rmul__(self, other):
+    def __rmul__(self : Vector, other):
         return(self.__mul__(other))
-    def squaredMagnitude(self):
+    @property
+    def squaredMagnitude(self : Vector):
         temp = 0
         for i in range(self.dim):
             temp += self[i]**2
         return temp
-    def magnitude(self):
-        return sqrt(self.squaredMagnitude())
-    def __abs__(self):
-        return self.magnitude()
-    def __invert__(self):
-        return self.squaredMagnitude()
-    def normalized(self):
-        return self/abs(self)
+    @property
+    def magnitude(self : Vector):
+        return sqrt(self.squaredMagnitude)
+    def __abs__(self : Vector):
+        return self.magnitude
+    def __invert__(self : Vector):
+        return self.squaredMagnitude
+    @property
+    def normalized(self : Vector):
+        return self / abs(self)
     def normalize(self):
         self /= abs(self)
     def __getattr__(self, attr):
@@ -199,8 +203,15 @@ class Vector():
             return self.array[AXIES[attr]]
         else:
             raise AttributeError("does not exist")
-    def __xor__(self, other):
-        self.innerProduct(other)
+    def scalerProjection(self : Vector, other : Vector):
+        return innerProduct(self,other)/other.magnitude()
+    def vectorProjection(self : Vector, other : Vector):
+        return (innerProduct(self,other)/other.squaredMagnitude())*other
+    def vectorProject(self : Vector, other : Vector):
+        self.array = ((innerProduct(self,other)/other.squaredMagnitude())*other).array
+        return self
+    def AngleBetweenVectors(self : Vector,other : Vector):
+        return acos(innerProduct(self,other)/(self.magnitude*other.magnitude))
 
 
 class Vector2(Vector):
@@ -208,14 +219,17 @@ class Vector2(Vector):
         c1 = x if x!=None else 0.0
         c2 = y if y!=None else x
         super().__init__([c1,c2])
-    def crossProduct(self, other):
+    def crossProduct(self : Vector, other : Vector):
         return (self.x*other.y) - (self.y*other.x)
-    def __pow__(self, other):
-        self.crossProduct(other)
-    def __rpow__(self, other):
-        self.__pow__(other)
-    def __ipow__(self, other):
-        self = self**other
+    def __pow__(self : Vector2, other : Vector2):
+        return self.crossProduct(other)
+    def __rpow__(self : Vector2, other : Vector2):
+        return self.__pow__(other)
+    def __ipow__(self : Vector2, other : Vector2):
+        self.array = (self**other).array
+        return self
+    def toComplex(self):
+        return Complex(self.x,self.y)
 class Vector3(Vector):
     def __init__(self, x : Optional[REAL], y : Optional[REAL], z : Optional[REAL]):
         c1 = x if x!=None else 0.0
@@ -227,12 +241,13 @@ class Vector3(Vector):
         return Vector3(self.y*other.z-self.z*other.y,
                        self.z*other.x-self.x*other.z,
                        self.x*other.y-self.y*other.x)
-    def __pow__(self, other):
-        self.crossProduct(other)
-    def __rpow__(self, other):
-        self.__pow__(other)
-    def __ipow__(self, other):
-        self = self**other
+    def __pow__(self : Vector3, other : Vector3):
+        return self.crossProduct(other)
+    def __rpow__(self : Vector3, other : Vector3):
+        return self.__pow__(other)
+    def __ipow__(self : Vector3, other : Vector3):
+        self.array = (self**other).array
+        return self
     
     
 def crossProduct(self : Vector2|Vector3, other : Vector2|Vector3):
@@ -244,19 +259,32 @@ def crossProduct(self : Vector2|Vector3, other : Vector2|Vector3):
                            self.x*other.y-self.y*other.x)
         else:
             raise ArithmeticError("undefined Opration")
-def innerProduct(self, other):
+def innerProduct(self : Vector, other : Vector):
     if(isinstance(other,Vector)):
         temp = 0
         for i in range(self.dim):
             temp += self[i]*other[i]
     return temp
-def squaredMagnitude(self):
-    return ~self
-def magnitude(self):
-    return abs(self)
-def normalized(self):
-    return self/abs(self)
-def normalize(self):
+def squaredMagnitude(self : Vector):
+    return self.squaredMagnitude
+def magnitude(self : Vector):
+    return self.magnitude
+def normalized(self : Vector):
+    return self.normalized
+def normalize(self : Vector):
     self /= abs(self)
 
+def scalerProjection(self : Vector, other : Vector):
+    return innerProduct(self,other)/other.magnitude()
+def vectorProjection(self : Vector, other : Vector):
+    return (innerProduct(self,other)/other.squaredMagnitude())*other
+def vectorProject(self : Vector, other : Vector):
+    self.array = ((innerProduct(self,other)/other.squaredMagnitude())*other).array
+    return self
 
+def AngleBetweenVectors(self : Vector,other : Vector):
+    return acos(innerProduct(self,other)/(self.magnitude*other.magnitude))
+def addScaledVector(self : Vector, other : Vector, scale : REAL):
+    return self + scale*other
+def perpendicularToVector(self : Vector, other: Vector):
+    return (innerProduct(self,other) == 0.0)
--- test.py
+++ test.py
@@ -1,9 +1,5 @@
 import simplemathlib as sml
 
-v1 = sml.Vector2(3.0,4.0)
-v2 = sml.Vector2(5,0)
-print(v1)
-print(v2)
-print(v2.normalized())
-print(v1.innerProduct(v2))
-print(v1.crossProduct(v2))
+v1 = sml.Vector2(0.0,4.0)
+v1.normalize()
+print(v1.y)
